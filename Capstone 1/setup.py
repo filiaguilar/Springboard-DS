38efac85efb36780ee4e347a88e890b86d292ff0
filiaguilar <64447099+filiaguilar@users.noreply.github.com>
2020-09-19 10:12:05 -0500

    Add files via upload

---
+++ Capstone 1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='According to the Centers for Disease Control and Prevention (CDC) smoking is the leading cause of preventable deaths in the United States. Not only is smoking very detrimental to one's health, smoking is also a very addictive. In an attempt to reduce the number of deaths attributed to smoking, this project aims to help anti-substance abuse organizations identify people who are smokers or are at high risk of picking up the habit based on their personality profiles.',
+    author='Filiberto Aguilar',
+    license='MIT',
+)
